c59cfe724f41ee8e4417e2f02611290b2b662620
yelisdurmus <durmus.yeliz95@gmail.com>
2020-05-07 17:04:45 +0300

    /api/directors Validation

--- models/Director.js
+++ models/Director.js
@@ -3,9 +3,21 @@ const Schema=mongoose.Schema;
 //const {MongoClient} = require('mongodb');
 
 const DirectorSchema=new Schema({
-    name:String,
-    surname:String,
-    bio:String,
+    name: {
+        type:String,
+        maxlength:60,
+        minlength:2
+    },
+    surname:{
+        type:String,
+        maxlength:60,
+        minlength:2
+    },
+    bio:{
+        type:String,
+        maxlength:1000,
+        minlength:60
+    },
     createdAt:{
         type: Date,
         default: Date.now()
--- models/Movie.js
+++ models/Movie.js
@@ -6,12 +6,30 @@ const MovieSchema=new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type: String,
-        required: true
+        required: [true,'`{PATH}` alanı zorunludur.'],
+        maxlength:[15,'`{PATH}` alanı (`{`VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
+        minlength:[1,'`{PATH}` alanı (`{`VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type:String,
+        maxlength: 30,
+        minlength: 1
+    },
+    country: {
+        type:String,
+        maxlength: 30,
+        minlength: 1
+    },
+    year: {
+        type:Number,
+        max:2040,
+        min: 1900
+    },
+    imdb_score: {
+        type:Number,
+        max:10,
+        min:0
+    },
     createdAt:{
         type: Date,
         default: Date.now()
